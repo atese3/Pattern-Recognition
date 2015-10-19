fe37d934591aaecbfa19a31ba5be7fafbc431232
Emre Ates <emreates26@gmail.com>
2015-10-19 21:01:12 +0300

    Revert "Pattern Recognition"
    This reverts commit 8992db75d304df48474c38b86704769a2f513ef2.

--- Classification/gaussian_dist.m
+++
@@ -1,9 +0,0 @@
-function [ p ] = gaussian_dist( x , m , s )
-   % Author Emre Ates
-   % x : test matrix
-   % m : mean value of the class
-   % s : variance value of the class
-   % Calculating Probabilty with Gaussian 2D Equation
-   p = (1/(sqrt(2*pi*det(s))))*exp(-0.5*(x-m)'*inv(s)*(x-m));
-end
-
